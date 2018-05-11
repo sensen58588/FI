fb98300046d55278c7de00f1f009e24f179931bc
sensen58588 <hz58588@foxmail.com>
2018-05-11 11:04:46 +0800

    最新版本TEST2

--- Test2/main/main.ino
+++ Test2/main/main.ino
@@ -1,53 +1,37 @@
 /*
   农田小气候分析仪程序入口
 */
-#include <Wire.h>
+#include <Wire.h> 
 #include <math.h>
-#include <dht11Bean.h>
-#include <GY30.h>
+#include <dht11Bean.h> //导入DHT11空气温湿度传感器
+#include <GY30.h> //导入GY30光敏数据传感器
+#include <RM5.h>  //导入RM5土壤湿度传感器
+#include <MQ2.h> //导入MQ2气体传感器
 
 dht11Bean DHT;
-
-/*
-  读取土壤湿度，读取值范围为234（水中）—1023（空气中）
-*/
-const int RM5 = 33;
-void getGroudHum() {
-  //Serial.println("土壤湿度传感器模块：");
-  float data = analogRead(0);
-  int key_status = digitalRead(RM5); //1：未达到湿度要求
-  if (key_status == 1)
-  {
-    float humidty = (1 - (data - 223) / 800) * 100;
-    Serial.println("土壤过于干燥");
-    Serial.print("湿度： ");
-    Serial.print(humidty);
-    Serial.println("%");
-  }
-  else
-  {
-    Serial.println("土壤湿度达标");
-    float humidty = (1 - (data - 223) / 800) * 100;
-    Serial.print("湿度： ");
-    Serial.print(humidty);
-    Serial.println("%");
-  }
-}
 
 void setup()
 {
   Wire.begin();
   Serial.begin(9600);
   Serial.println();
+  setHumidtyThreshold(40); //设置土壤湿度阈值（百分比）
+  setRM5PinNumber(0); //设置土壤湿度传感器模拟数据端口引脚为0
+  initMQ2(3,52); //初始化MQ2传感器的A0,D0引脚
 }
 
 void loop()
 {
   Serial.println("\n");
-  getGroudHum();
+  getGroudHum();//输出土壤湿度数据
+  Serial.println();
   DHT.setPin(23); //设置温湿度传感器的数据传输引脚
-  DHT.getHumAndTemp(1);
-  getLux();
+  DHT.getHumAndTemp(1); //默认输出不含额外信息
+  Serial.println();
+  getLux(); //输出光照强度
+  Serial.println();
+  getMQ2Data(); //输出甲烷，天然气气体检测数据
+ 
   delay(2000);
 }
 
